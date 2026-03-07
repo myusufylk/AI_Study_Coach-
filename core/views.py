fed1c47361c02d2f99a721fd062c94deed51262e
Yusuf Yılıkoğlu <yusuf.y0079@icloud.com>
2026-03-07 20:14:02 +0300

    veritabanı düzenlendi

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
